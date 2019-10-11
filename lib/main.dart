5c5cb175ccabf7be4e20f7349550f3a7bb42d08a
githumbi <githumbi74@gmail.com>
2019-10-11 19:35:45 +0300

    added functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Ask Me anything'),
+          ),
+          body: magicBall(),
+        ),
       ),
     );
+
+class magicBall extends StatefulWidget {
+  @override
+  _magicBallState createState() => _magicBallState();
+}
+
+class _magicBallState extends State<magicBall> {
+  @override
+  int ball = 1;
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(child:
+          FlatButton(onPressed: () {
+            setState(() {
+              ball = Random().nextInt(5) + 1;
+              print('button clicked');
+            });
+          },
+              child:  Image.asset('images/ball$ball.png')))
+
+        ],
+      ),
+    );
+  }
+}
